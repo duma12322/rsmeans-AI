3951fb65e294ed4c45ddf5d6c324145b3ef2ba0d
duma12322 <anais_asdm@hotmail.com>
2026-05-26 15:51:35 -0400

    fix UI selector + remove input manual + improve scraper

--- app/crawler.py
+++
@@ -1,7 +0,0 @@
-from crawl4ai import AsyncWebCrawler
-
-
-async def extract_content(url: str):
-    async with AsyncWebCrawler() as crawler:
-        result = await crawler.arun(url=url)
-        return result.markdown
